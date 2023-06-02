f7a5270b81cd1c7238e830e7a45e1ee8e00af9cd
KBHarshavardhan <harsha260820@gmail.com>
2023-06-02 11:24:50 +0530

    update folder

---
+++ src/main/java/com/vitesco/check/App.java
@@ -0,0 +1,13 @@
+package com.vitesco.check;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
